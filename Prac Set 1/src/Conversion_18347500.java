d97646b96a7e0465d8cc24b8702c06acad6651aa
Jared <steiner.jared1996@gmail.com>
2016-08-16 13:51:07 +1000

    Conversion outputs moved to methods menu selection if statements now point to:  - showKilometres  - showInches  - showFeet
    Also menu selection has been made into a switch case statement to cut down on lines

--- Prac Set 1/src/Conversion_18347500.java
+++ Prac Set 1/src/Conversion_18347500.java
@@ -11,77 +11,80 @@ public class Conversion_18347500 {
 
   // New keyboard input instance
   public static Scanner kb = new Scanner(System.in);
-  
+
   public static void main(String[] args) {
-    
+
     // SET VARS
     final double KM_PER_M = 0.001;      // KM Conversion rate
     final double INCH_PER_M = 39.37;    // INCH Conversion rate
     final double FEET_PER_M = 3.281;    // FOOT Conversion rate
-    
-    // Variables
-    double kilometres;          // KM Conversion of metre input
-    double inches;              // Inch Conversion of metre input
-    double feet;                // Feet Conversion of metre input
-    
+
     // MAIN CODE
-    int menuSelection = 0;   // Necessary for the while statements 
+    int menuSelection = 0;   // Necessary for the while statements
     do {
-      
+
       double iterationMetres = getDistance();        // gets Distance from user
-      
-      // Create conversions
-      kilometres = iterationMetres*KM_PER_M;         // create KM conversion
-      inches = iterationMetres*INCH_PER_M;           // create inches conversion
-      feet = iterationMetres*FEET_PER_M;             // create feet conversion
-      
+
       do {
         printMenu();                   // Prints menu options
         do {                           // menuSeletion is Int and 1 <= x <= 5
           menuSelection = (int)userDoubleInput("Enter a menu choice between 1 and 5: ");
         } while (menuSelection < 1 || menuSelection > 5);
-        
-        if (menuSelection == 1) {      // IF KILOMETRES
-          printLine(iterationMetres + " metres is " + kilometres + " kilometres.");
-          printLine("");
+
+        // MENU SELECTION CONVERSION/RETRY/EXIT
+        switch (menuSelection) {
+          case 1: showKilometres(iterationMetres, KM_PER_M);    // IF KILOMETRES
+                  break;
+          case 2: showInches(iterationMetres, INCH_PER_M);      // IF INCHES
+                  break;
+          case 3: showFeet(iterationMetres, FEET_PER_M);        // IF FEET
+                  break;
         }
-        
-        else if (menuSelection == 2) { // IF INCHES
-          printLine(iterationMetres + " metres is " + inches + " inches.");
-          printLine("");
-        }
-        
-        else if (menuSelection == 3) { // IF FEET
-          printLine(iterationMetres + " metres is " + feet + " feet.");
-          printLine("");
-        }
-        
+
       } while ( menuSelection < 4); // While conversion asked, repeat
-      
+
     } while (menuSelection != 5);   // While new distance asked, repeat
-    
+
   }
-  
+
   static double userDoubleInput(String inputMessage) {
     System.out.print(inputMessage);
     return kb.nextDouble();
   }
-  
+
   // Gets the metres distance from user
   static double getDistance() {
     double inputMetres;                 // Local metres variable for method
     do {
       inputMetres = userDoubleInput("Enter a distance in metres: ");
     } while (inputMetres < 0.0);
-    
+
     return inputMetres;
   }
-  
+
+  public static void showKilometres(double metres, double KM_PER_M) {
+    double kilometres = metres*KM_PER_M;         // create KM conversion
+    printLine(metres + " metres is " + kilometres + " kilometres.");
+    printLine("");
+  }
+
+  public static void showInches(double metres, double INCH_PER_M) {
+    double inches = metres*INCH_PER_M;           // create INCH conversion
+    printLine(metres + " metres is " + inches + " inches.");
+    printLine("");
+  }
+
+  public static void showFeet(double metres, double FEET_PER_M) {
+    double feet = metres*FEET_PER_M;             // create FEET conversion
+    printLine(metres + " metres is " + feet + " feet.");
+    printLine("");
+  }
+
   // Quicker println() statement
   static void printLine(String message) {
     System.out.println(message);
   }
-  
+
   // Prints out menu options
   static void printMenu() {
     printLine("1. Convert to Kilometres");
