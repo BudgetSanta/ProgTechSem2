77423122f1389f57e29232015ed430bdd504383f
Jared <steiner.jared1996@gmail.com>
2016-08-21 21:54:08 +1000

    In prep for this weeks programming Q's (Blank.java)

---
+++ WK6 Prac/src/Blank.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Blank {
+
+  public static void main(String[] args) {
+
+    // Variables
+
+    // New keyboard input instance
+    Scanner kb = new Scanner(System.in);
+
+    // End keyboard input instance
+    kb.close();
+  }
+
+}
