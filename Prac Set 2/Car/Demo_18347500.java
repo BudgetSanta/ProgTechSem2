7538b5d697f37e3dae18e08d9a9ed2939630e960
Jared <steiner.jared1996@gmail.com>
2016-09-06 12:31:53 +1000

    Revised Demo Demo:  - When validating INTs, 'error please enter again' message shown' with while loop  - Shortened some lines, put in n characters  - Validation no longer takes Strings, only ints  - User input prompts a little clearer

--- Prac Set 2/Car/Demo_18347500.java
+++ Prac Set 2/Car/Demo_18347500.java
@@ -12,16 +12,16 @@ public class Demo_18347500 {
 
   public static Scanner kb = new Scanner(System.in);
 
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) {
 
     // assign cars from carCreation()
     Car[] garage = carCreation();       // Three car output from carCreation
 
     // INVENTORY OUTPUT
-    System.out.println("OUTPUT: Cars in invetory");
-    getCar(garage[0]);
-    getCar(garage[1]);
-    getCar(garage[2]);
+    System.out.println("\n\nOUTPUT: Cars in invetory");
+    getCar(garage[0], "Car One");
+    getCar(garage[1], "Car Two");
+    getCar(garage[2], "Car Three");
 
     // DEMONSTRATIONS
     garage[0] = accelDemo("Car One", garage[0]);
@@ -29,165 +29,137 @@ public class Demo_18347500 {
     garage[1] = setterDemo("Car Two", garage[1]);
 
     // FINAL OUTPUT BEFORE TEMINATION
-    System.out.println("OUTPUT: Cars in invetory");
-    getCar(garage[0]);
-    getCar(garage[1]);
-    getCar(garage[2]);
+    System.out.println("\n\nOUTPUT: Cars in invetory");
+    getCar(garage[0],"Car One");
+    getCar(garage[1],"Car Two");
+    getCar(garage[2],"Car Three");
   }
 
   // Print car details
-  public static void getCar(Car carInst) {
-    System.out.println(" ----- CAR ----- ");
-    System.out.println(" make: " + carInst.getMake());
-    System.out.println(" year: " + carInst.getYear());
-    System.out.println("speed: " + carInst.getSpeed());
+  public static void getCar(Car carInst, String carName) {
+    System.out.println(" ----- " + carName + " ----- ");
+    System.out.println(" Make: " + carInst.getMake());
+    System.out.println(" Year: " + carInst.getYear());
+    System.out.println("Speed: " + carInst.getSpeed());
   }
 
   // ##### INPUTS #####
   // ###################
 
   // Shorter String input
-  public static String getStr(String prompt) throws IOException{
+  public static String getStr(String prompt) {
     System.out.print(prompt);
     return kb.next();
   }
 
   // Shorter VALIDATED Int input
-  public static Integer getInteger(String prompt) throws IOException{
-    // Takes string input to validate as Int
-    try {
-      String outStr = "";
-      do {
-        outStr = getStr(prompt);
-      } while (!isInteger(outStr));
-      return Integer.parseInt(outStr);
-    } catch (Exception e) {             // IF EXCEPTION THROWN, 0 RETURNED
-      System.out.println("Error while parsing Integer. INTEGER:0 returned from getInteger");
-      return 0;
+  public static int getInt(String prompt) {
+    int outputInt = -1;        // while loop continues until value changed to pos int
+    while (!isPosInt(outputInt)) {
+      System.out.print(prompt);
+      outputInt = kb.nextInt();
+      if (!isPosInt(outputInt)) {
+        System.out.println("## ERROR: Number not positive. Please enter positive integer. ##");
+      }
     }
+    return outputInt;
   }
 
-  static Car[] carCreation() throws IOException{
-
+  static Car[] carCreation() {
     Car[] garage = new Car[3];              // Car Object Array to store cars
-
     // CAR CREATION
     System.out.println("Please enter the following details below for car creation");
 
-    // ### CAR ONE ###
-    // Car one uses the blank constructor
+    // CAR ONE uses the blank constructor
     Car carOne = new Car();
-    garage[0] = carOne;               // Assigning car to garage slot
+    System.out.println("\nCar One created with blank constructor!");
+    garage[0] = carOne;
 
-    // ### CAR TWO ###
-    // Car two uses a completely user generated constructor
-    System.out.println("MAKE, YEAR AND SPEED");
+    // CAR TWO uses a completely user generated constructor
+    System.out.println("\nCar Two - Make, Year and Speed");
     Car carTwo = new Car(
       getStr("What is this car's make? "),
-      getInteger("What year was this car made? "),
-      getInteger("What is this car's current speed? ")
+      getInt("What year was this car made? "),
+      getInt("What is this car's current speed? ")
     );
-    garage[1] = carTwo;               // Assigning car to garage slot
+    garage[1] = carTwo;
 
-    // ### CAR THREE ###
-    // Car three uses a semi user generated constructor setting speed to 0
-    System.out.println("MAKE AND YEAR");
+    // CAR THREE uses a semi user generated constructor setting speed to 0
+    System.out.println("\nCar Three - Make and Year");
     Car carThree = new Car(
       getStr("What is this car's make? "),
-      getInteger("What year was this car made? ")
+      getInt("What year was this car made? ")
     );
-    garage[2] = carThree;            // Assigning car to garage slot
-
+    garage[2] = carThree;
     return garage;
 
   }
 
   // DEMOs Acceleration mutators
-  static Car accelDemo(String carName, Car car) throws IOException{
+  static Car accelDemo(String carName, Car car) {
 
     // ACCELERATE DEMO. speed, static accel, speed, var accel, speed
-    System.out.println("");
-    System.out.println("DEMO: accelerate mutators");
-    System.out.println("Car One Speed: " + car.getSpeed());
+    System.out.println("\n\nDEMO: accelerate mutators");
+    System.out.println(carName + " Speed: " + car.getSpeed());
 
     // First Accel
     System.out.println("Preset Accelerating by 5km/h...");
     car.accelerate();
-    System.out.println("Car One Speed: " + car.getSpeed());
+    System.out.println(carName + " Speed: " + car.getSpeed());
 
     // Second Accel
-    int userInputAccel;                 // User defined acceleration speed
-    userInputAccel = getInteger("Enter acceleration speed (Integer): ");
+    int userInputAccel = getInt("Enter acceleration speed (Integer): ");     // User defined acceleration speed
     car.accelerate(userInputAccel);
     System.out.println("Accelerating by " +  userInputAccel + " km/h...");
 
     // Final Speed
-    System.out.println("Car One Speed: " + car.getSpeed());
-
+    System.out.println(carName + " Speed: " + car.getSpeed());
     return car;
   }
 
   // DEMOs Brake mutators
-  static Car brakeDemo(String carName, Car car) throws IOException{
+  static Car brakeDemo(String carName, Car car) {
 
     // BRAKE DEMO. speed, static brake, speed, var brake, speed
-    System.out.println("");
-    System.out.println("DEMO: brake mutators");
-    System.out.println("Car One Speed: " + car.getSpeed());
+    System.out.println("\n\nDEMO: brake mutators for " + carName);
+    System.out.println(carName + " Speed: " + car.getSpeed());
 
     // First Brake
     System.out.println("Preset Braking...");
     car.brake();
-    System.out.println("Car One Speed: " + car.getSpeed());
+    System.out.println(carName + " Speed: " + car.getSpeed());
 
     // Second Brake
-    int userInputBrake;                   // User defined brake speed
-    userInputBrake = getInteger("Enter brake speed (Integer): ");
+    int userInputBrake = getInt("Enter brake speed (Integer): ");     // User defined brake speed
     car.brake(userInputBrake);
     System.out.println("Braking by " +  userInputBrake + "km/h...");
 
     // Final Speed
-    System.out.println("Car One Speed: " + car.getSpeed());
-
+    System.out.println(carName + " Speed: " + car.getSpeed());
     return car;
 
   }
 
   // DEMOs attribute setters
-  static Car setterDemo(String carName, Car car) throws IOException{
-
+  static Car setterDemo(String carName, Car car) {
     // SETTER DEMO. attributes, set attributes, attributes
-    System.out.println("");
-    System.out.println("DEMO: Individual setters");   // Much better than settlers
-    System.out.println("...... " + carName + " ......");
-    getCar(car);
+    System.out.println("\n\nDEMO: Individual setters for " + carName);   // Much better than settlers
+    getCar(car, carName);
 
-    car.setMake(getStr("Set Make: "));
-
-    // Set car year with int validation
-    car.setYear(getInteger("Set Year: "));
-
-    // Set car speed with int validation
-    car.setSpeed(getInteger("Set Speed: "));
-
+    car.setMake(getStr(" Set Make: "));    //Set Car Make
+    car.setYear(getInt(" Set Year: "));    // Set car year with int validation
+    car.setSpeed(getInt("Set Speed: "));  // Set car speed with int validation
     // Output car attributes
-    getCar(car);
-
+    getCar(car, carName);
     return car;
-
   }
 
-  // Validates Strings as Integers
-  static boolean isInteger(String strNum) throws IOException{
-    try {
-      // parse string as int
-      Integer integerNum = Integer.parseInt(strNum);
-      // if it doesn"t throw an error, return TRUE
+  // returns true for ints > 0
+  static boolean isPosInt(int num) {
+    if (num >= 0) {
       return true;
     }
-    catch (NumberFormatException nFE) {
-      System.out.println("");
-      System.out.println(" ##NumberFormatException: Please enter an Integer");
+    else {
       return false;
     }
   }
